cebaead4d308a011a367fd0f5016ff524e6c6c92
NNooeeFF <58855987+NNooeeFF@users.noreply.github.com>
2020-10-25 10:33:32 +0100

    Update speedclick.js

--- speedclick.js
+++ speedclick.js
@@ -1,37 +1,61 @@
 
 let result = 10
+let level = 1
+let time = 0
 
 alert ("Vous allez jouer à un jeu addictif, continuer ?");
 
 
-var myVar = setInterval(lessOne, 1000);
+var myVar = setInterval(printResult, 1000);
+var myVar = setInterval(timer, 1000);
 
+
+function timer(){
+	if (result < 100){
+		time += 1;
+	} else {
+		time += 0;
+	}
+}
+
+
+function endLevel(){
+	document.getElementById("titleText").innerHTML = "Bravo, vous avez réussi en " + time + " secondes ! Passer au niveau suivant ?";
+	document.getElementById("buttonText").innerHTML = "Next =>";
+}
+
+function nextLevel(){
+	window.open ('level2.htm','_self',false)
+}
 
 function addOne(){
 	if (result < 100){
 		result += 1;
-		document.getElementById("printResultat").innerHTML = result;
+		document.getElementById("textResultat").innerHTML = result;
 	} else {
 		result += 0;
+		setTimeout(nextLevel, 800);
 	}
 }
 
-function lessOne(){
+function printResult(){
 	if (result < 30) {
 		result -= 1;
-		document.getElementById("printResultat").innerHTML = result;
+		document.getElementById("textResultat").innerHTML = result;
 	} else if (result >= 30 && result < 45){
 		result -= 3;
-		document.getElementById("printResultat").innerHTML = result;
+		document.getElementById("textResultat").innerHTML = result;
 	} else if (result >= 45 && result < 60){
-		result -= 4;
-		document.getElementById("printResultat").innerHTML = result;
+		result -= 1/*4;*/
+		document.getElementById("textResultat").innerHTML = result;
 	} else if (result >= 60 && result < 95){
-		result -= 5;
-		document.getElementById("printResultat").innerHTML = result;
+		result -= 1 /*5;*/
+		document.getElementById("textResultat").innerHTML = result;
 	} else if (result >= 95 && result < 100){
-		result -= 7;
-		document.getElementById("printResultat").innerHTML = result;
+		result -= 1; /*7;*/
+		document.getElementById("textResultat").innerHTML = result;
+	} else if (result = 100){
+		setTimeout(endLevel, 800);
 	}
 }
 
