a9133e4e770c5e72a98a37384071086705d9ecbe
NNooeeFF <58855987+NNooeeFF@users.noreply.github.com>
2020-10-25 11:58:44 +0100

    Create speedclick2.js

---
+++ speedclick2.js
@@ -0,0 +1,66 @@
+
+let result = 11
+let level = 1
+let time = 0
+let readyToLeave = false
+
+alert ("Pour ce niveau, vous aurez besoin d'un 2e pouce, ready ?");
+
+
+var myVar = setInterval(printResult, 1000);
+var myVar = setInterval(timer, 1000);
+
+
+function timer(){
+	if (result < 100){
+		time += 1;
+	} else {
+		time += 0;
+	}
+}
+
+
+function endLevel(){
+	readyToLeave = true;
+}
+
+function nextLevel(){
+	window.open ('level2.htm','_self',false)
+}
+
+
+function addOne(){
+	if (result < 100){
+		result += 1;
+		document.getElementById("textResultat").innerHTML = result;
+	} else {
+		result += 0;
+		/*if (readyToLeave = true) {
+			nextLevel();
+		}*/
+	}
+}
+
+function printResult(){
+	if (result < 30) {
+		result -= 1;
+		document.getElementById("textResultat").innerHTML = result;
+	} else if (result >= 30 && result < 45){
+		result -= 3;
+		document.getElementById("textResultat").innerHTML = result;
+	} else if (result >= 45 && result < 60){
+		result -= 4;
+		document.getElementById("textResultat").innerHTML = result;
+	} else if (result >= 60 && result < 95){
+		result -= 5;
+		document.getElementById("textResultat").innerHTML = result;
+	} else if (result >= 95 && result < 100){
+		result -= 7;
+		document.getElementById("textResultat").innerHTML = result;
+	} else if (result = 100){
+		document.getElementById("titleText").innerHTML = "Bravo, vous avez réussi en " + time + " secondes !";
+		/*document.getElementById("buttonText").innerHTML = "Next =>";
+		setTimeout(endLevel, 1800);*/
+	}
+}
+
